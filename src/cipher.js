243bbf32a38b48628c576a5fe26cb260d57c97ac
AuraMelisa01 <auramelisa6@gmail.com>
2018-06-14 20:16:48 +0200

    Validacion_espacios_mayusculas_minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-//window.cipher = {
 
+//window.cipher = {
 //};
+
+
 
 const  cifradoCesar =(texto,clave) =>{
  texto=document.getElementById("textoMensaje").value;
@@ -32,8 +33,10 @@ const  cifradoCesar =(texto,clave) =>{
       }
 
 
-    }
+  }
 
+     document.getElementById("mensajeCifradoDescifrado").innerHTML=cifradoCont;
+}
 
       /*  if (parseInt(texto[i])% 1 === 0){     //limita la entrada de numeros
            alert("No se admiten numeros, solo texto");
@@ -41,31 +44,11 @@ const  cifradoCesar =(texto,clave) =>{
 
 
     }*/
-=======
-/*window.cipher = {
-  // ...
-};*/
 
-function cifradoCesar(){
 
-let texto=document.getElementById("textoMensaje").value;
-let clave=document.getElementById("keyPosicion").value;
 
-let cifradoCont="";
 
-   for (let i=0; i<texto.length; i++){
 
-     let numAsq=texto.toUpperCase().charCodeAt(i) - 65 + clave % 26 + 65; // Formula: Obtiene el numero de la letra en ASQII
-     let mensajeDescifrado=String.fromCharCode(numAsq); // Cifra el numero y lo convierte a letra
-     cifradoCont+= mensajeDescifrado;  //Guarda las letras obtenidas
-
-    }
->>>>>>> df7be67d7be5d3f68acd104ee451e00eaaafdc8f
-    document.getElementById("mensajeCifradoDescifrado").innerHTML=cifradoCont; //Imprime las letras obtenidas
-
- }
-
-<<<<<<< HEAD
 // cifradoCesar();
 
 
@@ -76,7 +59,7 @@ const descifradoCesar = (texto, clave) => {
 
   let descifradoCont="";
 
-alert("No se aceptan numeros");
+
    for (let i=0; i<texto.length; i++){
      if (texto.charCodeAt(i)===32){ //agrega si hay espacios
         let espacio=" ";
@@ -93,45 +76,16 @@ alert("No se aceptan numeros");
      }
 
      else if(texto.charCodeAt(i) >=97 && texto.charCodeAt(i)<=122){  // Minusculas
-       console.log(texto);
+       //console.log(texto);
        let numAsq=texto.charCodeAt(i) + 97 - clave % 26 - 97; // Formula: Obtiene el numero de la letra en ASQII
-       console.log(numAsq);
+       //console.log(numAsq);
        let mensajeCifrado=String.fromCharCode(numAsq); // Cifra el numero y lo convierte a letra
        descifradoCont+= mensajeCifrado;  //Guarda las letras obtenidas
      }
 
-    }
     document.getElementById("mensajeCifradoDescifrado").innerHTML=descifradoCont;
 
- }
-
-
-
-
-
- //descifradoCesar();
-=======
-cifradoCesar();
-
-
-function descifradoCesar(){
-
-  let texto=document.getElementById("textoMensaje").value;
-  let clave=document.getElementById("keyPosicion").value;
-
-  let descifradoCont="";
-
-   for (let i=0; i<texto.length; i++){
-     let numAsq=(texto.toUpperCase().charCodeAt(i) + 65 - clave) % 26 + 65; // Formula: Obtiene el numero de la letra en ASQII
-     let mensajeDescifrado=String.fromCharCode(numAsq); // Descrifra el numero y lo convierte a letra
-
-    descifradoCont+= mensajeDescifrado;  //Guarda las letras obtenidas
-
-    }
-    document.getElementById("mensajeCifradoDescifrado").innerHTML=descifradoCont; //Obtiene lo guardado de la variable descifradoCont
-                                                                                //  y lo imprime en la etiqueta mensajeCifradoDescifrado
+  }
 
  }
-
- descifradoCesar();
->>>>>>> df7be67d7be5d3f68acd104ee451e00eaaafdc8f
+//descifradoCesar();
